2ea67d63baf2d5ba08502cd7446c678b506df3dd
Logan Wood <lwood3499@gmail.com>
2019-01-08 15:20:54 -0600

    increasing performance by checking modal for update needs

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 import classes from './Modal.module.css';
 
-const modal = props => {
-    return (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.modalClosed} />
-            <div
-                className={classes.Modal}
-                style={{
-                    transform: props.show
-                        ? 'translateY(0)'
-                        : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}
-            >
-                {props.children}
-            </div>
-        </Aux>
-    );
-};
+class Modal extends Component {
+    // We could also use PureComponent, but this would do more checks than we need for this
+    // application
+    // we are testing to see if Modal needs to update, so that we aren't rerendering unecessarily
+    // NOTE:  the WRAPPING element controls the updating of the WRAPPED element
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate');
+    }
+    render() {
+        return (
+            <Aux>
+                <Backdrop
+                    show={this.props.show}
+                    clicked={this.props.modalClosed}
+                />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show
+                            ? 'translateY(0)'
+                            : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default Modal;
