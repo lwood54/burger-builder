08bbfc1013e441e0e3164dba697677536dc4f974
Logan Wood <lwood3499@gmail.com>
2019-01-10 19:05:52 -0600

    cleaned up console logs

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,7 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4,
+        totalPrice: 3,
         purchasable: false,
         purchasing: false
     };
