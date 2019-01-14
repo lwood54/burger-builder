ae725fcd6432d17d71f4bb0d07d9f38f750242d6
Logan Wood <lwood3499@gmail.com>
2019-01-14 08:38:17 -0600

    adding a contact data component and styles

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
-import classes from './CheckoutSummar.module.css';
+import classes from './CheckoutSummary.module.css';
 
 const checkoutSummary = props => {
     return (
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes great!</h1>
             <div style={{ width: '100%', margin: 'auto' }}>
+                {' '}
+                {/* SOMETHING WRONG WITH BURGER STYLE HERE */}
                 <Burger ingredients={props.ingredients} />
             </div>
             <Button btnType="Danger" clicked={props.checkoutCanceled}>
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from '../ContactData/ContactData';
 
 export class Checkout extends Component {
     state = {
@@ -39,6 +42,10 @@ export class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutContinued={this.checkoutContinuedHandler}
                     checkoutCanceled={this.checkoutCanceledHandler}
+                />
+                <Route
+                    path={this.props.match.path + '/contact-data'}
+                    component={ContactData}
                 />
             </div>
         );
